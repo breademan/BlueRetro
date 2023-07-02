53b64812d29fadb13d022e5bdb334d6fd95902ee
breademan <infectedfwiss@gmail.com>
2023-07-02 16:11:57 +0900

    Memory card appears functional, pending more tests

--- main/wired/maple.c
+++ main/wired/maple.c
@@ -626,38 +626,36 @@ maple_end:
                                 pkt.data32[27] = PWR_VMU;
                                 maple_tx(port, maple0, maple1, pkt.data, pkt.len * 4 + 5);
                                 break;
-                            case CMD_EXT_INFO_REQ: //TODO unimplemented
+                            case CMD_EXT_INFO_REQ: //unimplemented
                             case CMD_GET_CONDITION:
                             case CMD_MEM_INFO_REQ:
                                 pkt.len = 0x07;
                                 pkt.cmd = CMD_DATA_TX;
                                 pkt.data32[0] = ID_VMU_MEM;
-                                //media information: 24 bytes
                                 memcpy((void *)&pkt.data32[1], vmu_media_info, sizeof(vmu_media_info));
                                 maple_tx(port, maple0, maple1, pkt.data, pkt.len * 4 + 5);
                                 break;
-                            case CMD_BLOCK_READ://TODO untested
-                                pkt.len = 0x82; //d128+2
+                            case CMD_BLOCK_READ:
+                                pkt.len = 0x82;
                                 pkt.cmd = CMD_DATA_TX;
                                 pkt.data32[0] = ID_VMU_MEM;
                                 //pkt.data32[1] // This should be the same as what the host sent, though this also applies for [0].
-                                phase = (uint8_t) ((pkt.data32[1] >> 8) & 0x00FF);
+                                phase = (uint8_t) ((pkt.data32[1] >> 16) & 0x00FF);
                                 if(phase) {ets_printf("Block Read with unexpected phase: 0x%02X, expected 0\n", phase);}
-                                block_no = (uint8_t) ((pkt.data32[1] >> 16) & 0x00FF);
-                                mc_read(block_no*512, &pkt.data32[2],512);
+                                block_no = (uint8_t) ((pkt.data32[1]) & 0x00FF);
+                                mc_read(block_no*512, (void *) &pkt.data32[2],512);
                                 maple_tx(port, maple0, maple1, pkt.data, pkt.len * 4 + 5);
                                 break;
-                            case CMD_BLOCK_WRITE://TODO untested
-                                //Verify my assumption that write accesses is actually 4
+                            case CMD_BLOCK_WRITE:
                                 if(pkt.len!=32+2){ets_printf("Unexpected Block Write packet length: 0x%02X, expected 0x22\n", pkt.len);}
                                 pkt.len = 0x00;
                                 pkt.cmd = CMD_ACK;
                                 if ((!bad_frame) && pkt.data32[0]==ID_VMU_MEM) {
-                                    phase = (uint8_t) ((pkt.data32[1] >> 8) & 0x00FF);
-                                    block_no = (uint8_t) ((pkt.data32[1] >> 16) & 0x00FF);
+                                    phase = (uint8_t) ((pkt.data32[1] >> 16) & 0x00FF);
+                                    block_no = (uint8_t) ((pkt.data32[1]) & 0x00FF);
                                     //data might be written to the VMU scrambled in wire order; this should make compatability with other devices wrong,
                                     //but if we read data back in the same order it should be OK until I make an unscramble function.
-                                    mc_write((block_no*512)+(128*phase),&pkt.data32[2],128);
+                                    mc_write((block_no*512)+(128*phase),(void *) &pkt.data32[2],128);
                                 }
                                 maple_tx(port, maple0, maple1, pkt.data, pkt.len * 4 + 5);
                                 break;
